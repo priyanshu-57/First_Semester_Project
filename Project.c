ff099b73fe5f8d0b3542625454aa1fdace138a45
Priyanshu Kushawaha <kushawahapriyanshu57@gmail.com>
2023-09-17 00:55:22 +0545

    Bug fix

--- Project.c
+++ Project.c
@@ -4,7 +4,7 @@
 #include<process.h>
 #include<string.h>
 #include<stdlib.h>
-#include<dos.h>
+//#include<dos.h>
 
 void login() {
     char username[] = "user";
@@ -201,7 +201,7 @@ main:
 
                 {
 
-                    printf("\n%d. \nName\t: %s\nPhone\t: %ld\nAddress\t: %s\nEmail\t: %s\n",i-96,list.name,list.ph,list.add,list.email);
+                    printf("\nName\t: %s\nPhone\t: %ld\nAddress\t: %s\nEmail\t: %s\n",i-96,list.name,list.ph,list.add,list.email);
                     found++;
 
                 }
